bd940de754c9224585a3b5598e93dfaa147194e9
QuangThuP9 <phamquangthu2@gmail.com>
2020-12-30 23:32:44 +0700

    initial commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // import
 const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
 const app = express()
 const port = 3000
 
@@ -7,16 +8,19 @@ const port = 3000
 // static files
 app.use(express.static('public'))
 app.use('/css', express.static(__dirname+'public/css'))
-app.use('/js', express.static(__dirname+'public/js'))
-app.use('/images', express.static(__dirname+'public/images'))
+// app.use('/js', express.static(__dirname+'public/js'))
+// app.use('/images', express.static(__dirname+'public/images'))
 
 
 
 // set views
-app.set('views', './views')
+// app.set('views', './views')
+app.use(expressLayouts)
+app.set('layout','./layouts/full-width')
 app.set('view engine', 'ejs')
 
 
+// navigation
 app.get('', (req, res)=>{
     // res.sendFile(__dirname+ '/views/index.html')
     //res.render('index')
